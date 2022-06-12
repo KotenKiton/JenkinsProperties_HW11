07d1547f9b85745ad063dfe3545d146dd776aa08
Koten <Kotenkiton@gmail.com>
2022-06-12 13:20:54 +0300

    first commit

---
+++ src/test/java/demoqa/tests/TestBase.java
@@ -0,0 +1,46 @@
+package demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+
+    @BeforeAll
+    static void setUp() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    
+        Configuration.baseUrl = "https://demoqa.com"; // Задать базовый УРЛ.
+        Configuration.browserSize = "1920x1080"; // задать желаемый размер экрана.
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; // путь запуска селеноида
+
+        // копипаста для подключения видео отчётов.
+        DesiredCapabilities capabilities = new DesiredCapabilities(); // capabilities - ключи и значения
+        capabilities.setCapability("enableVNC", true); // enableVNC ключ его значение true
+        capabilities.setCapability("enableVideo", true); // enableVideo - ключ его значение true
+        Configuration.browserCapabilities = capabilities;
+        // EnableVideo - делать записи или нет
+        // EnableVNC - транслировать ui или нет. т.е установив false не будет видно окна с тем что происходит.
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+
+
+}
+
+
